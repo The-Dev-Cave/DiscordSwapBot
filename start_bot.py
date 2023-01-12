98ebe0eba9520f88f5e3b3f5f7c98349c46fcf01
theonlydoublee <theonlydoublee@gmail.com>
2023-01-11 19:38:44 -0700

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -1,6 +1,11 @@
 import asyncio
+import os
 
 from BotCode.create_bot import create_bot
 
 if __name__ == "__main__":
+    if os.name != "nt":
+        import uvloop
+        print("UVLOOP")
+        uvloop.install()
     asyncio.run(create_bot()).run()
