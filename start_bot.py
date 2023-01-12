df8cb29b5f34362f935a837955c4e2f3f016d3bd
theonlydoublee <theonlydoublee@gmail.com>
2023-01-11 19:40:03 -0700

    Remove Print Statment in start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -6,6 +6,5 @@ from BotCode.create_bot import create_bot
 if __name__ == "__main__":
     if os.name != "nt":
         import uvloop
-        print("UVLOOP")
         uvloop.install()
     asyncio.run(create_bot()).run()
