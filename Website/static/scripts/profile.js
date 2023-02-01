2ccd040029fe4ca90476b4ce2ce14c46f34d4d5b
Zach329 <programmer329@gmail.com>
2023-02-01 10:19:38 -0700

    Fix Profile Maker Img Upload

--- Website/static/scripts/profile.js
+++ Website/static/scripts/profile.js
@@ -1,15 +1,19 @@
-document.getElementById("editBtn").onclick = function(){
+var editBtn = document.getElementById("editBtn")
+if (editBtn){
+    editBtn.onclick = function(){
     
-    var inputs = document.getElementsByTagName("input");
-    for (var i = 0; i < inputs.length; i++) {
-        if(inputs[i].disabled){
-            inputs[i].disabled = false;
-        } else {
-            inputs[i].disabled = true;
+        var inputs = document.getElementsByTagName("input");
+        for (var i = 0; i < inputs.length; i++) {
+            if(inputs[i].disabled){
+                inputs[i].disabled = false;
+            } else {
+                inputs[i].disabled = true;
+            }
         }
+    
     }
-
 }
+
 
 window.onload = function () {
     var fileupload = document.getElementById("FileUpload1");
