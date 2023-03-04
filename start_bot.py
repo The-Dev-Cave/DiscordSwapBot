0c2392f4bae79d6dd3567a47f78789287ec662bd
theonlydoublee <theonlydoublee@gmail.com>
2023-03-03 19:17:52 -0700

    Update start_bot.py

--- start_bot.py
+++ start_bot.py
@@ -8,5 +8,10 @@ from BotCode.create_bot import create_bot
 if __name__ == "__main__":
     if os.name != "nt":
         import uvloop
+
         uvloop.install()
-    asyncio.run(create_bot()).run(status=hikari.Status.DO_NOT_DISTURB)
+    asyncio.run(create_bot()).run(
+        status=hikari.Status.ONLINE, activity=hikari.Activity(
+            name="Marketing",
+            type=hikari.ActivityType.COMPETING,
+        ))
